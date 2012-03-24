c9c39e3085a1e4b2e093baee515de7e68674cb44
Joshua Holt <holt.josh@gmail.com>
2012-03-24 15:46:33 -0400

    Fixing a jslint error

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = process.env.WIKI_COV
-  ? require('./lib-cov/db.js')
-  : require('./lib/db.js');
+module.exports = process.env.WIKI_COV ? require('./lib-cov/db.js') : require('./lib/db.js');
